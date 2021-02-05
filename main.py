5d5b6fca830bb7477ab350299cb3e14faf885b2d
Mullonch <thomas.mullor@gmail.com>
2021-02-05 09:43:45 +0100

    legers ajouts

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from utils.JobShop import *
 
 js = JobShop()
 
-sol = js.pick_default_solution()
-toto = sol.inv_blocks_of_critical_path()
-print(toto)
+sol = js.heuristique_gloutonne("EST_SPT")
+print(sol.duration)
+print(sol.new_neighbors())
